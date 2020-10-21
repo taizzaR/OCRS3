99c3007143226c56841ca8592e91fb3e93d2af5f
Alexis Duchet <alexis.duchet@epita.fr>
2020-10-21 18:47:01 +0200

    Neural network test

--- NeuralNetwork/neural_network_XOR.c
+++ NeuralNetwork/neural_network_XOR.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include "tools.h"
+#include <math.h>
+
+struct Neuron{
+	double output;
+	int weightsLength;
+	double bias;
+	double delta //Je sais pas trop ce que cest
+	double inputs[2];
+	double weights[2];
+};
 
 struct Neural_Network Initialization()
 {
     struct Neural_Network network;
     network.biais0 = Random();
+}
+
+double init_weight() {return ((double)rand())/((double)RAND_MAX);
+
+double xor(double x, double y):
+	if (x == y){
+		return 0;
+	}
+	return 1;
 }
